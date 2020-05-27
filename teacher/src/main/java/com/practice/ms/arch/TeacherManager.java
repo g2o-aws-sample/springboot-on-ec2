61849779c87d15c97af15365a41559f578ff2f4f
vgohil <vgohil@icct.com>
2020-05-27 14:58:37 -0400

    sample spring boot app with docker and aws ec2

---
+++ teacher/src/main/java/com/practice/ms/arch/TeacherManager.java
@@ -0,0 +1,12 @@
+package com.practice.ms.arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherManager {
+
+    public static void main(String[] ar) {
+        SpringApplication.run(TeacherManager.class, ar);
+    }
+}
