e9a2bb8eafa02d541da06d1af05ff877537003cc
Ankit Solanki <ankit.solanki.net@hotmail.com>
2020-06-30 13:13:42 -0400

    Setting up React app

---
+++ multi-tier-app/reactapp/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "games-app-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
+        APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
+        IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    }
+};
