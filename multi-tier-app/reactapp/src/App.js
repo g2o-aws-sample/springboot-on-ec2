b1a3a8d5ec78aec98fe72de61a805d5fd2f8f5c1
Ankit Solanki <ankit.solanki.net@hotmail.com>
2020-07-02 14:43:07 -0400

    Login Page changes

--- multi-tier-app/reactapp/src/App.js
+++ multi-tier-app/reactapp/src/App.js
@@ -1,33 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Routes from "./Routes";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
+import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
+import { onError } from "./libs/errorLib";
 
 
 function App() {
+    const [isAuthenticated, userHasAuthenticated] = useState(false);
+    const [isAuthenticating, setIsAuthenticating] = useState(true);
+    const history = useHistory();
+
+    useEffect(() => {
+        onLoad();
+    }, []);
+
+    async function onLoad() {
+        try {
+            await Auth.currentSession();
+            userHasAuthenticated(true);
+        }
+        catch (e) {
+            if (e !== 'No current user') {
+                onError(e);
+            }
+        }
+
+        setIsAuthenticating(false);
+    }
+
+    async function handleLogout() {
+        await Auth.signOut();
+
+        userHasAuthenticated(false);
+
+        history.push("/login");
+    }
+
     return (
+        !isAuthenticating &&
         <div className="App container">
             <Navbar fluid collapseOnSelect>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <Link to="/">gameOn</Link>
+                        <Link to="/">Scratch</Link>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav pullRight>
-                        <LinkContainer to="/signup">
-                            <NavItem>Signup</NavItem>
-                        </LinkContainer>
-                        <LinkContainer to="/login">
-                            <NavItem>Login</NavItem>
-                        </LinkContainer>
+                        {isAuthenticated
+                            ? <NavItem onClick={handleLogout}>Logout</NavItem>
+                            : <>
+                                <LinkContainer to="/signup">
+                                    <NavItem>Signup</NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/login">
+                                    <NavItem>Login</NavItem>
+                                </LinkContainer>
+                            </>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-            <Routes />
+            <AppContext.Provider
+                value={{ isAuthenticated, userHasAuthenticated }}
+            >
+                <Routes />
+            </AppContext.Provider>
         </div>
     );
 }
--- multi-tier-app/reactapp/src/config.js
+++ multi-tier-app/reactapp/src/config.js
@@ -1,16 +1,16 @@
 export default {
     s3: {
-        REGION: "us-east-1",
-        BUCKET: "games-app-uploads"
+        REGION: "us-east-2",
+        BUCKET: "games-g2o-app-uploads"
     },
     apiGateway: {
-        REGION: "us-east-1",
+        REGION: "us-east-2",
         URL: "YOUR_API_GATEWAY_URL"
     },
     cognito: {
-        REGION: "us-east-1",
-        USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-        APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-        IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_2gB7VDE94",        
+        IDENTITY_POOL_ID: "us-east-2:31780860-596c-4747-b3df-b814aed787cc",
+        APP_CLIENT_ID: "5fcd2per8hn2gvi8m9tfj75uj0"
     }
 };
